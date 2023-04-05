0cfaf58e192b75a5d4d217bc4b06f0036a341234
Sakshi Gupta <sakshi.k425@gmail.com>
2023-04-05 19:43:06 -0400

    updated main terraform file

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ locals {
 
 data "azurerm_subscription" "current" {
 }
+data "azurerm_client_config" "current" {}
 
 // Random Suffix Generator
 
@@ -41,4 +42,34 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 
   tags = local.tags
+}
+
+// Machine Learning
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = ""${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}keyvault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
+
+resource "azurerm_machine_learning_workspace" "mlws" {
+  name                    = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}-workspace"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.appinsights.id
+  key_vault_id            = azurerm_key_vault.kv.id
+  storage_account_id      = azurerm_storage_account.storage.id
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
